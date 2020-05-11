c217f7b2a0adb502b9b03362f06bd983c45549db
Michael Currin <michael.currin@gmail.com>
2020-05-11 20:35:11 +0200

    refactor: Improve hex number handling

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import redis
 import tweepy
 
 
-auth = tweepy.OAuthHandler(os.environ["CONSUMER_TOKEN"], os.environ["CONSUMER_SECRET"]) # Place consumer_token and consumer_secret
-auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"]) # Place access_token and access_token_secret
+# This is stored as an `int`.
+MAX_HEX = 0xffffff
+
+
+auth = tweepy.OAuthHandler(os.environ["CONSUMER_TOKEN"], os.environ["CONSUMER_SECRET"])
+auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
 api = tweepy.API(auth)
 
 r = redis.Redis(
@@ -16,14 +20,11 @@ r = redis.Redis(
     password=os.environ["PASSWORD"]
 )
 
-def main():
-    global color_number
-    while color_number < 16777216: # Till last possible hex number
-        hex_number = str(hex(color_number))[2:]
-        format_length = 6 - len(hex_number)
 
-        for i in range(format_length): #Adds 0 if needed
-            hex_number = "0" + hex_number
+def main(color_number=0):
+    while color_number < MAX_HEX:
+        # 6-digit zero padded hex string. e.g. "012def"
+        hex_number = f"{color_number:06x}"
 
         media_url = f"http://www.singlecolorimage.com/get/{hex_number}/500x500.png"
         try:
@@ -32,14 +33,14 @@ def main():
             print(f"Hex value: {hex_number}")
             raise
 
-        api.update_with_media("temp_image.jpg", status="#" + hex_number)
+        api.update_with_media("temp_image.jpg", status=f"#{hex_number}")
         print(media_url)
         r.incr('num')
         color_number = int(r.get("num"))
-        time.sleep(15 * 60) # Posts every 15 minutes
-
+        # Posts every 15 minutes.
+        time.sleep(15 * 60)
 
 
 if __name__ == "__main__":
     color_number = int(r.get("num"))
-    main()
+    main(color_number)
